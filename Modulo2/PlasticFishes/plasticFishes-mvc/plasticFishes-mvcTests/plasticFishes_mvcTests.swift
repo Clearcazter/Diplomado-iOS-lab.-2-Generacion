a613c54e18016149b1842b778dca169a53f4dd60
Clearcazter <36734799+Clearcazter@users.noreply.github.com>
2018-09-14 18:26:49 -0500

    Commit

---
+++ Modulo2/PlasticFishes/plasticFishes-mvc/plasticFishes-mvcTests/plasticFishes_mvcTests.swift
@@ -0,0 +1,34 @@
+//
+//  plasticFishes_mvcTests.swift
+//  plasticFishes-mvcTests
+//
+//  Created by Guerrero Azpitarte Adrian on 14/09/18.
+//  Copyright © 2018 Guerrero Azpitarte Adrian. All rights reserved.
+//
+
+import XCTest
+@testable import plasticFishes_mvc
+
+class plasticFishes_mvcTests: XCTestCase {
+    
+    override func setUp() { //antes de que comience a correr las pruebas
+        super.setUp()
+    }
+    //Quieres que haga pruebas.
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
